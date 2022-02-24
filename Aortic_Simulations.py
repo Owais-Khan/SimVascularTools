54c64d7e6e7e5593901e59a22fad50fdb3c6e6e0
Muhammad Owais Khan <khanmu11@nia-login01.scinet.local>
2022-02-24 08:57:48 -0500

    updated script

--- Aortic_Simulations.py
+++ Aortic_Simulations.py
@@ -8,27 +8,30 @@ import numpy as np
 import math
 from scipy.optimize import minimize
 pConv = 1333.34
+USER_EMAIL_ADDRESS="owaiskhan@ryerson.ca"
 PRE_SOLVER_PATH = '/home/k/khanmu11/ana/Softwares/svSolver/BuildWithMake/Bin/svpre.exe'
 SOLVER_PATH ='/home/k/khanmu11/ana/Softwares/svSolver/BuildWithMake/Bin/svsolver-openmpi.exe'
 POST_SOLVER_PATH = '/home/k/khanmu11/ana/Softwares/svSolver/BuildWithMake/Bin/svpost.exe'
+RUN_COMMAND='srun'
 FLOWSOLVER_NODES=2
 PROCESSORS=40
+CPUs=FLOWSOLVER_NODES*PROCESSORS
 BATCH_COMMAND="sbatch "
 class AorticSimulations():
-	def __init__(self)
+	def __init__(self):
 		#Patient specific Assigned Parameters
 		self.Pao_min=24 #minimum diastolic pressure (mmHg)
 		self.Pao_max=43.5 #maximum systolic pressure (mmHg)
 		
 		#Solver paramters
-		self.Ntimesteps=200
-		self.Ncycles=4
+		self.Ntimesteps=1000
+		self.Ncycles=2
 		self.Nrestart=50
 
 		#Some constant parameters that don't change for each patient
 		self.Pao_mean=0.333*self.Pao_max + 0.666*self.Pao_min
-		self.E_Aorta=700000 #elastic modulus of the aorta
-		self.E_AorticBranches=700000
+		self.E_Aorta=665890.2229 #elastic modulus of the aorta
+		self.E_AorticBranches=self.E_Aorta*2.6 #Ratio from Table2 of Coogan et al. (2013 BMM)
 		self.nuvw=0.5 #poisson ratio
 		self.density=1.0 #tissue density
 		self.kcons=0.83333 #shear constant
@@ -41,7 +44,7 @@ class AorticSimulations():
 		self.C_AorticBranches=self.C_Aorta/10.
 		
 		#Get the number of caps
-		self.CapNames=sorted(glob("./mesh-complete/mesh-surfaces/cap_*.vtp"))
+		self.CapNames=sorted(glob.glob("./mesh-complete/mesh-surfaces/cap_*.vtp"))
 
 		#Get the period from the flow file
 		infile=open("inflow.flow")
@@ -53,35 +56,46 @@ class AorticSimulations():
 				self.Period=float(line[0])
 				self.FlowRate+=float(line[1])
 				counter+=1.
-		self.FlowRate\=counter
+		self.FlowRate/=(counter*-1)
 		print ("The Flow Rate is: %.05f"%self.FlowRate)
 		print ("The Period is: %.05f"%self.Period)
 
 
 		#The total resistance
 		self.Rtot=(self.Pao_mean*pConv)/self.FlowRate
-		print ("The total resistance is: %.05"%self.Rtot)
+		print ("The total resistance is: %.05f"%self.Rtot)
 
 	def Main(self):
 		#Read the Flow Splits for each of the outlet
+		print ("Getting Flow Splits")
 		FlowSplits=self.ReadFlowSplits()
 		FlowSplits_assigned=FlowSplits
-
-		#Generate the presolver file 
+		
+		#Generate the presolver file
+		print ("Writing Presolver File") 
 		self.WritePreSolveFile()
 
 		#Generate the Solver file
+		print ("Writing Solver File")
 		self.WriteSolverFile()
+		
+		#Create numstart file 
+		infile=open("numstart.dat",'w')
+		infile.write("0")
+		infile.close()
 
 		#Run presolver
-		os.system(PRE_SOLVER_PATH+" AortaFSI.svpre")
+		#os.system(PRE_SOLVER_PATH+" AortaFSI.svpre")
 		
 		Error=100 #Error in flow splits
-		while Error>3	
+		Iteration=0.
+		while Error>3:	
 			#Write rcr file
 			self.WriteRCRFile(FlowSplits_assigned)
 
 			#Run FSI Simulation
+			if len(glob.glob("%s-procs_case"%CPUs))>0:
+				os.system("mv %s-procs_case Try%d_%s-procs_case"%(CPUs,Iteration,CPUs))
 			self.Run3DSIM()
 
 			#Compute flow split and mean pressures for last cycle
@@ -93,66 +107,67 @@ class AorticSimulations():
 			#Computed Flow Splits
 			SumQ=0
 			FlowSplits_computed={}
-			for CaseName_ in self.CaseNames: SumQ+=FlowRates[CapName_]
-			for CaseName_ in self.CaseNames: 
-				FlowSplits_computed[CaseName_]=FlowRates[CapName_]/SumQ
+			for CapName_ in self.CapNames: SumQ+=FlowRates[CapName_]
+			for CapName_ in self.CapNames: 
+				FlowSplits_computed[CapName_]=FlowRates[CapName_]/SumQ
 	
 		
 			#Print the assigned and computed flow splits
 			Error=0
-			for CaseName_ in self.CaseNames:
-				Q_split_com=100*FlowSplits_computed[CaseName_]
-				Q_split_ass=100*FlowSplits_assigned[CaseName_]
+			for CapName_ in self.CapNames:
+				Q_split_com=100*FlowSplits_computed[CapName_]
+				Q_split_ass=100*FlowSplits_assigned[CapName_]
 				print ("\n")
-				print ("Q_assigned for %s: %.03f"%(CaseName_,Q_split_com))
-				print ("Q_computed for %s: %.03f"%(CaseName_,Q_split_ass))
+				print ("Q_assigned for %s: %.03f"%(CapName_,Q_split_ass))
+				print ("Q_computed for %s: %.03f"%(CapName_,Q_split_com))
 				Error+=abs(Q_split_com-Q_split_ass)
 	
 			print ("-"*20)
 			print ("The cumulative error in flow split is: %.03f"%Error)
-	
+			print ("Current Iterations is: %d"%(Iteration))	
 			#Update the flow splits
-			for CaseName_ in self.CaseNames:
-				FlowSplits_assigned[CaseName_]*=(FlowSplits[CaseName_]/FlowSplits_computed[CaseName_])
-	
+			for CapName_ in self.CapNames:
+				FlowSplits_assigned[CapName_]*=(FlowSplits[CapName_]/FlowSplits_computed[CapName_])
+				print (FlowSplits_assigned[CapName_])	
+			Iteration+=1
 	def ComputeAverage(self,Parameter,nStart,nStop):
-		CPUs=FLOWSOLVER_NODES*PROCESSORS
 		if Parameter=="Q":
-			infile=open("%d-procs_case/QHistRCR.dat.%d"%(CPUs,self.Ncycles*self.Ntimesteps))
+			infile=open(sorted(glob.glob("%d-procs_case/QHist*.dat.%d"%(CPUs,nStop)))[-1],'r')
 		elif Parameter=="P":
-			infile=open("%d-procs_case/PHistRCR.dat.%d"%(CPUs,self.Ncycles*self.Ntimesteps))
+			infile=open(sorted(glob.glob("%d-procs_case/PHist*.dat.%d"%(CPUs,nStop)))[-1],'r')
 		else:
 			print ("Neither Pressure nor Flow Rate was defined for 3D Post-processing")
-			print ("Exiting..."0
+			print ("Exiting...")
 			exit(1)
 
-		infile.readline()
+		print(infile.readline())
 		AverageParameter={}
 		for CapName_ in self.CapNames: AverageParameter[CapName_]=0
-		counter=0
+		counter=nStart
 		for LINE in infile:
 			line=LINE.split()
-			if counter>nStart and counter<=nStop: 
+			if counter>=nStart and counter<nStop: 
 				for i in range(len(self.CapNames)):
 					AverageParameter[self.CapNames[i]]+=float(line[i])
 				counter+=1
 	
-		for CaseName_ in self.CaseNames: AverageParameter[CaseName_]/=counter
+		for CapName_ in self.CapNames: AverageParameter[CapName_]/=counter
+		infile.close()
 		return AverageParameter
 	
 
 	def Run3DSIM(self):
-		self.WriteJobScript("Niagara_FlowSolver", 'svFlowsolver', 2, FLOWSOLVER_NODES, PROCESSORS)
-		flow_script = open("NiagaraFlowSolver", 'a')
+		self.WriteJobScript("Niagara_FlowSolver", 'svFlowsolver', 5, FLOWSOLVER_NODES, PROCESSORS)
+		flow_script = open("Niagara_FlowSolver", 'a')
 		flow_script.write('\n')
 		flow_script.write(RUN_COMMAND + ' '+SOLVER_PATH+"\n")
 		flow_script.close()	
 		
 		#If there is no procs-folder, then run a 3D simulation
-		procs_list = glob.glob('*-procs_*')
+		procs_list = glob.glob('%d-procs_*'%CPUs)
 		if(len(procs_list) == 0):
 			print("\n** Starting 3D SimVascular Simulation\n")
-			command_string = BATCH_COMMAND +  "Niagara_FlowSolver"
+			command_string = BATCH_COMMAND + " "+ "Niagara_FlowSolver"
 			print(command_string)
 			os.system(command_string)
 		else:
@@ -162,7 +177,7 @@ class AorticSimulations():
 
   
 		# NEED FLAG HERE TO DETERMINE STOP OF 3D SIMULATION
-		total_steps = self.Ntimesteps*Ncycles
+		total_steps = self.Ntimesteps*self.Ncycles
 		procs_list = glob.glob('%d-procs_case'%(int(FLOWSOLVER_NODES*PROCESSORS)))
 		print('\n** Waiting for simulation job to launch...\n')
 
@@ -179,18 +194,17 @@ class AorticSimulations():
 		AllData_file=glob.glob('%d-procs_case/AllData'%(int(FLOWSOLVER_NODES*PROCESSORS)))
 		while(len(AllData_file) == 0):
 			time.sleep(180)
-			AllData_file = glob.glob('%d-procs_case/AllData'%(int(FLOWSOLVER_NODES*PROCESSORS)))
-			print('\n** AllData file has been written...\n')
+			AllData_file = glob.glob('%d-procs_case/histor.dat'%(int(FLOWSOLVER_NODES*PROCESSORS)))
+			print('\n** histor.dat has been written...\n')
 
   
 		#Check to see if the Simulation is finished
 		sim_finished = False
 		while(not sim_finished):
 			time.sleep(100)
-			AllData_check = open(sim_folder + '/AllData', 'r')
-			counter = 0
-			for line in AllData_check:
-				counter = counter + 1
+			AllData_check = open(sim_folder + '/histor.dat', 'r')
+			for line in AllData_check: counter=int(line.split()[0])
+
 			print ("------- Finished %d of %d timesteps"%(counter,total_steps))
 			AllData_check.close()
 			if(counter == total_steps):
@@ -226,43 +240,48 @@ class AorticSimulations():
 		scriptFile.close()
 
 	def WriteRCRFile(self,FlowSplits):
-		rcr_file=open("rcr.dat".'w')
+		rcr_file=open("rcrt.dat",'w')
 		rcr_file.write("2\n")
+		#Get the sum of flow split
+		for CapName_ in self.CapNames: print (CapName_)
+		QSum=sum([FlowSplits[CapName_] for CapName_ in self.CapNames])
+		
 		for CapName_ in self.CapNames:
 			rcr_file.write("2\n")
-			rcr_file.write("%.08f\n"%(1./FlowSplits[CapName_]*0.09))
-			if CapName_.find("aorta"): rcr_file.write("%.08f\n"%self.C_Aorta)
+			rcr_file.write("%.08f\n"%(QSum/FlowSplits[CapName_]*0.09*self.Rtot))
+			if CapName_.find("aorta")>=0: rcr_file.write("%.08f\n"%self.C_Aorta)
 			else: rcr_file.write("%.08f\n"%self.C_AorticBranches)
-			rcr_file.write("%.08f"%(1./FlowSplits[CapName_]*0.91))
+			rcr_file.write("%.08f\n"%(QSum/FlowSplits[CapName_]*0.91*self.Rtot))
 			rcr_file.write("0.0 0\n")
 			rcr_file.write("1.0 0\n")	
-
+		rcr_file.close()
+		
 	def WriteSolverFile(self):
 		solver_file=open("solver.inp",'w')
 	
 		solver_file.write("Density: 1.06\n")
 		solver_file.write("Viscosity: 0.04\n")
-
+		solver_file.write("\n")
 		solver_file.write("Number of Timesteps: %d\n"%(self.Ncycles*self.Ntimesteps))
 		solver_file.write("Time Step Size: %.08f\n"%(self.Period/self.Ntimesteps))
-
-		solver_file.write("Number of Timesteps between Restarts: %d\n"%(self.Nrestarts))
+		solver_file.write("\n")
+		solver_file.write("Number of Timesteps between Restarts: %d\n"%(self.Nrestart))
 		solver_file.write("Number of Force Surfaces: 1\n")
 		solver_file.write("Surface ID's for Force Calculation: 1\n")
 		solver_file.write("Force Calculation Method: Velocity Based\n")
 		solver_file.write("Print Average Solution: True\n")
 		solver_file.write("Print Error Indicators: False\n")
-
+		solver_file.write("\n")
 		solver_file.write("Time Varying Boundary Conditions From File: True\n")
-		solver_file.write("Step Construction: 0 1 0 1\n")
-
-		solver_file.write("Number of RCR Surfaces: %d"%len(self.CapNames))
+		solver_file.write("\n")
+		solver_file.write("Step Construction: 0 1 0 1 0 1 0 1\n")
+		solver_file.write("\n")
+		solver_file.write("Number of RCR Surfaces: %d\n"%len(self.CapNames))
 		solver_file.write("List of RCR Surfaces: ")
 		for i in range(len(self.CapNames)): solver_file.write("%d "%(i+2))
 		solver_file.write("\n")
-
 		solver_file.write("RCR Values From File: True\n")
-
+		solver_file.write("\n")
 		solver_file.write("Deformable Wall: True\n")
 		solver_file.write("Variable Wall Thickness and Young Mod: True\n")
 		solver_file.write("Density of Vessel Wall: 1\n")
@@ -270,7 +289,7 @@ class AorticSimulations():
 		solver_file.write("Shear Constant of Vessel Wall: 0.833333\n")
 		solver_file.write("Pressure Coupling: Implicit\n")
 		solver_file.write("Number of Coupled Surfaces: %d\n"%(len(self.CapNames)))
-
+		solver_file.write("\n")
 		solver_file.write("Backflow Stabilization Coefficient: 0.2\n")
 		solver_file.write("Residual Control: True\n")
 		solver_file.write("Residual Criteria: 1e-5\n")
@@ -278,10 +297,16 @@ class AorticSimulations():
 		solver_file.write("svLS Type: NS\n")
 		solver_file.write("Number of Krylov Vectors per GMRES Sweep: 200\n")
 		solver_file.write("Number of Solves per Left-hand-side Formation: 1\n")
+		"""solver_file.write("Tolerance on Momentum Equations: 1e-5\n")
+		solver_file.write("Tolerance on Continuity Equations: 1e-5\n")
+		solver_file.write("Tolerance on svLS NS Solver: 1e-5\n")
+		solver_file.write("Maximum Number of Iterations for svLS NS Solver: 5\n")
+		solver_file.write("Maximum Number of Iterations for svLS Momentum Loop: 5\n")
+		solver_file.write("Maximum Number of Iterations for svLS Continuity Loop: 1000\n")"""
 
 	def WritePreSolveFile(self):
 		svpre_file = open('AortaFSI.svpre','w')
-		svpre_file.write('mesh_and_adjncy_vtu ' + mesh_complete + '\n')
+		svpre_file.write('mesh_and_adjncy_vtu mesh-complete/mesh-complete.mesh.vtu\n')
  
 		#Set Surface Id
 		svpre_file.write('set_surface_id_vtp mesh-complete/mesh-complete.exterior.vtp 1\n')
@@ -330,7 +355,7 @@ class AorticSimulations():
 			if CapName_.find("aorta")>=0:
 				svpre_file.write("set_surface_E_vtp %s %.05f\n"%(CapName_,self.E_Aorta))
 			else:
-				svpre_file.write("set_surface_E_vtp %s %.05f\n"(CapName_,self.E_AorticBranches))
+				svpre_file.write("set_surface_E_vtp %s %.05f\n"%(CapName_,self.E_AorticBranches))
 			
 		#Solve for Elastic Modulus
 		svpre_file.write("solve_varwall_E\n")	
@@ -340,7 +365,7 @@ class AorticSimulations():
 		svpre_file.write("varwallprop_write_vtp varwallprop.vtp\n")
 		svpre_file.write("deformable_nu 0.5\n")
 		svpre_file.write("deformable_kcons 0.833333\n")
-		svpre_file.write("deformable_pressure %.05f\n")
+		svpre_file.write("deformable_pressure %.05f\n"%self.Pao_mean)
 		svpre_file.write("deformable_solve_varwall_displacements\n")
 		svpre_file.write("wall_displacements_write_vtp displacement.vtp\n")
 		svpre_file.write("write_geombc geombc.dat.1\n")
@@ -350,27 +375,35 @@ class AorticSimulations():
 
 	def GetWallThickness(self):
 		Thickness={}
-		outlets=sorted(glob("./mesh-complete/mesh-surfaces/cap_*.vtp"))
+		outlets=sorted(glob.glob("./mesh-complete/mesh-surfaces/cap_*.vtp"))
 		for outlet in outlets:
 			SurfaceArea_=self.GetSurfaceArea(outlet)
 			Thickness[outlet]=0.1*np.sqrt(SurfaceArea_/np.pi)
 		return Thickness	
 	
 	#This function computes the surface area of the inlet or the outlets
-	def GetSurfaceArea(Surface):
+	def GetSurfaceArea(self,Surface):
 		masser = vtk.vtkMassProperties()
-		masser.SetInputData(Surface)
+		masser.SetInputData(self.ReadVTPFile(Surface))
 		masser.Update()
 		return masser.GetSurfaceArea()
+
+	def ReadVTPFile(self,FileName):
+		reader=vtk.vtkXMLPolyDataReader()
+		reader.SetFileName(FileName)
+		reader.Update()
+		return reader.GetOutput()
 
 	#This function will read user defined flow splits inside the file
 	def ReadFlowSplits(self):
 		infile=open(self.FlowSplitFileName,'r')
 		#First colum contains outletname and the second outlet contains flow split
 		FlowSplits={}
-		for Line in FlowSplits:
+		for Line in infile:
 			line=Line.split()
-			FlowSplits["./mesh-complete/mesh-surfaces/"+line[0]]=float(line[1])
+			CaseName_=glob.glob("./mesh-complete/mesh-surfaces/*%s*"%line[0])[0]
+			FlowSplits[CaseName_]=float(line[1])
 		infile.close()
-
-		
+		return FlowSplits
+if __name__=="__main__":
+	AorticSimulations().Main()		
